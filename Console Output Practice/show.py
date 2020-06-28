3b6c3518b3df8ed23d8d148cdd72f40ffc981773
Ify <Zasterify@gmail.com>
2020-06-28 19:39:07 -0400

    print_whole

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -7,3 +7,4 @@ name = "Damiete Oruwari"  # To preserve the varaiable 'full name'
 age = "21 years old" # To save the variable 'age'
 country = "Nigeria" # To preserve the variable 'country'
 college = "Gallaudet University" # To save the variable 'college'
+print("My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and also attend Gallaudet University.") # To print the full statement. 
