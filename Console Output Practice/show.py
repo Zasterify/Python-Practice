a494c718e92ef8173aa204dd095cc90566d706b7
Ify <Zasterify@gmail.com>
2020-06-28 19:35:58 -0400

    variable_name

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -1,3 +1,7 @@
 print ("My name is Damiete") # To print my name
 print ("I am from Nigeria") # To print my country 
 print ("I attend Gallaudet University") # To print my college
+
+
+name = "Damiete Oruwari"  # To preserve the varaiable 'full name'
+
