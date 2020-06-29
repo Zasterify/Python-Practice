ff99b7a5a10f1cefecf5f4781dd77681e5fdf075
Ify <Zasterify@gmail.com>
2020-06-29 16:41:03 -0400

    first_lastName (print)

--- Console Output Practice/name.py
+++ Console Output Practice/name.py
@@ -1,3 +1,4 @@
 firstName = "Damiete " # To save the variable called 'firstName' 
 lastName = "Oruwari"  # To save the variable called 'lastname'
+print((firstName + lastName + ' ') * 5)  # To print the firstName and lastName multiplied by the number
 
