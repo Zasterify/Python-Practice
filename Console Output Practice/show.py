04617d90303af794700f662bd06699c6c4df229b
Ify <Zasterify@gmail.com>
2020-06-29 12:32:35 -0400

    variable_college updated

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -5,4 +5,4 @@ print ("I attend Gallaudet University") # To print my college
 name = "Damiete Oruwari" and "Foluso Adeluola" # To preserve the variable called 'name'
 age = "21 years old" and "18 years old" # To save the variable called 'age'
 country = "Nigeria" # To preserve the variable called 'country' for both
- 
+collge = "Gallaudet University" # To save the variable called 'college' for both 
