90eb0045d4df5295dc63c41e695b74fdadd3a773
Ify <Zasterify@gmail.com>
2020-06-29 16:39:09 -0400

    firstName (variable)

--- Console Output Practice/name.py
+++ Console Output Practice/name.py
@@ -1,3 +1,3 @@
-name = "Damiete" # To save the variable called 'name' 
-print((name + ' ') * 5)  # To print the name multiplied by the number
+firstName = "Damiete " # To save the variable called 'firstName' 
+
 
