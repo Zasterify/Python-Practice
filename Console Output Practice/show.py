ad3644171ab50d8d5b09e28b0fbcdb9d1e982130
Ify <Zasterify@gmail.com>
2020-06-29 13:50:18 -0400

    check - in

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -2,10 +2,15 @@ print ("My name is Damiete") # To print my name
 print ("I am from Nigeria") # To print my country 
 print ("I attend Gallaudet University") # To print my college
 
-name = "Damiete Oruwari" and "Foluso Adeluola" # To preserve the variable called 'name'
-age = "21 years old" and "18 years old" # To save the variable called 'age'
-country = "Nigeria" # To preserve the variable called 'country' for both
-collge = "Gallaudet University" # To save the variable called 'college' for both
+name = "Damiete Oruwari"   # To preserve the variable called 'name'
+age = "21" # To save the variable called 'age'
+country = "Nigeria" # To preserve the variable called 'country' 
+college = "Gallaudet University" # To save the variable called 'college' 
+print("My name is", name, "I am", age, "years old", "I am from ", country, "and i attend ", college) # To print the whole statement
 
-print("My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and i attend Gallaudet University") # To print the whole statement 
-print("My friend\'s name is Foluso Adeluola. He is 18 years old. He is from Nigeria and attend to Gallaudet University") # To print the whole statement for another one 
+name = "Foluso Adeluola" # To preserve the variable called 'name'
+age = "18 years old"  # To save the variable called 'age'
+country = "Nigeria"  # To preserve the variable called 'country'
+college = "Gallaudet University"  # To save the variable called 'college'
+print("My name is", name, "I am", age, "years old", "He is from ", country, "and also attend ", college) # To print the whole statement)
+
