f359468178c75da526b277d6acc6057a53e609e9
Ify <Zasterify@gmail.com>
2020-06-29 14:56:58 -0400

    Correction

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -1,5 +1,17 @@
-name = input("Enter your name: ") # To preserve the variable 'name'
-Rename = int(input("Enter a number: ")) # To repeat how many times the word is given 
-print((name + ' ') * Rename) # To print this to display the name 
+print("My name is Damiete")  # To print my name
+print("I am from Nigeria")  # To print my country
+print("I attend Gallaudet University")  # To print my college
 
+name = "Damiete Oruwari"   # To preserve the variable called 'name'
+age = "21"  # To save the variable called 'age'
+country = "Nigeria"  # To preserve the variable called 'country'
+college = "Gallaudet University"  # To save the variable called 'college'
+print("My name is", name, "I am", age, "years old", "I am from ",
+      country, "and i attend ", college)  # To print the whole statement
 
+name = "Foluso Adeluola"  # To preserve the variable called 'name'
+age = "18 years old"  # To save the variable called 'age'
+country = "Nigeria"  # To preserve the variable called 'country'
+college = "Gallaudet University"  # To save the variable called 'college'
+print("My name is", name, "I am", age, "years old", "He is from ",
+      country, "and also attend ", college)  # To print the whole statement)
