4844cc02a719e1e2c921be9a80c97b3a47b4ffb9
Ify <Zasterify@gmail.com>
2020-06-29 10:36:27 -0400

    updated variable_age

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -9,3 +9,4 @@ college = "Gallaudet University" # To save the variable 'college'
 print('My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and l attend to Gallaudet University') # To print the full statement 
 
 name = "Foluso Adeluola" # To preserve the variable called 'name'
+age = "18 years old" # To save the variable called 'age'
