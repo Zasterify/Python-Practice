f1d5e906ced4549223197a3d38e0a194676af972
Ify <Zasterify@gmail.com>
2020-06-29 14:30:47 -0400

    Print(_variable)

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -1,4 +1,5 @@
 name = input("Enter your name: ") # To preserve the variable 'name'
 Rename = int(input("Enter a number: ")) # To repeat how many times the word is given 
+print((name + ' ') * Rename) # To print this to display the name 
 
 
