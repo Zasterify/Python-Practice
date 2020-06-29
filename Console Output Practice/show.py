50732f67938d7a6fa91e3abb6d229f5e81100362
Ify <Zasterify@gmail.com>
2020-06-29 10:35:37 -0400

    updated variable_name

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -2,9 +2,10 @@ print ("My name is Damiete") # To print my name
 print ("I am from Nigeria") # To print my country 
 print ("I attend Gallaudet University") # To print my college
 
-
-name = "Damiete Oruwari"  # To preserve the varaiable 'full name'
+name = "Damiete Oruwari" # To preserve the variable 'full name'
 age = "21 years old" # To save the variable 'age'
-country = "Nigeria" # To preserve the variable 'country'
+country = "Nigeria"  # To preserve the variable 'country'
 college = "Gallaudet University" # To save the variable 'college'
-print("My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and also attend Gallaudet University.") # To print the full statement. 
+print('My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and l attend to Gallaudet University') # To print the full statement 
+
+name = "Foluso Adeluola" # To preserve the variable called 'name'
