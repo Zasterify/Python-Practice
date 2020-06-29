365a6eb11d2b44f634cbed1b35c4bc04fba8eb33
Ify <Zasterify@gmail.com>
2020-06-29 12:39:26 -0400

    print_whole updated (two)

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -7,4 +7,5 @@ age = "21 years old" and "18 years old" # To save the variable called 'age'
 country = "Nigeria" # To preserve the variable called 'country' for both
 collge = "Gallaudet University" # To save the variable called 'college' for both
 
-print("My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and i attend Gallaudet University") # To print the whole statement  
+print("My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and i attend Gallaudet University") # To print the whole statement 
+print("My friend\'s name is Foluso Adeluola. He is 18 years old. He is from Nigeria and attend to Gallaudet University") # To print the whole statement for another one 
