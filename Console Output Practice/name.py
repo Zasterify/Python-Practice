ff083f1d91233f17210d82472e60c4acaf286cd3
Ify <Zasterify@gmail.com>
2020-06-29 16:26:55 -0400

    Altered Name(_variable)

--- Console Output Practice/name.py
+++ Console Output Practice/name.py
@@ -1,3 +1,2 @@
-name = input("Enter your name: ") # To save the variable "name"
-Repeat  = int(input("Enter a number: ")) # To repeat how many times the name shown
-print((name + ' ') * Repeat) # To print the name
+name = "Damiete" # To save the variable called 'name' 
+print((name + ' ') * 5)  # To print the name multiplied by the number  
