936500e9670ef239438eec425ecf63680c4d2f8e
Ify <Zasterify@gmail.com>
2020-06-29 12:35:53 -0400

    print_whole  updated

--- Console Output Practice/show.py
+++ Console Output Practice/show.py
@@ -5,4 +5,6 @@ print ("I attend Gallaudet University") # To print my college
 name = "Damiete Oruwari" and "Foluso Adeluola" # To preserve the variable called 'name'
 age = "21 years old" and "18 years old" # To save the variable called 'age'
 country = "Nigeria" # To preserve the variable called 'country' for both
-collge = "Gallaudet University" # To save the variable called 'college' for both 
+collge = "Gallaudet University" # To save the variable called 'college' for both
+
+print("My name is Damiete Oruwari. I am 21 years old. I am from Nigeria and i attend Gallaudet University") # To print the whole statement  
