70cfc66fba6b18879db0f88556fc5a0cd00355b2
Ify <Zasterify@gmail.com>
2020-06-30 00:05:22 -0400

    print higher_age

--- Numbers Practice/marriage.py
+++ Numbers Practice/marriage.py
@@ -4,3 +4,4 @@ print("my age is ",myAge, " that means i can marry between the age of ",lower_ag
 
 myAge = 19  # To print your age
 higher_age_limit = int(myAge * 2) - 13 # To calculate it to get the answer of the higher age
+print("my age is ",myAge, "that means i can marry between the age of 18 and ",higher_age_limit, " years old")
