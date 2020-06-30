42247a667f6743df5c6eea236291f8a1b813ca3e
Ify <Zasterify@gmail.com>
2020-06-30 13:46:54 -0400

    fishes_print

--- Numbers Practice/fishing.py
+++ Numbers Practice/fishing.py
@@ -1,5 +1,5 @@
 number_fishes_in_pond = int(input("How many do you want to catch: "))  # To put the number of fishes in a pond as you like
 number_fishes_caught = int(input("Enter a number: "))  # To store the numbers of fishes you already caught
 total_fishes_left = (int(number_fishes_in_pond - number_fishes_caught))  # To count how many fishes are left
-
+print("There were ",number_fishes_in_pond, " fish(es) in a pond. " "You caught ",number_fishes_caught, "fish(es)." " There are now in ",total_fishes_left, " fishes left in the pond. HAppy fishing!") # To print the whole statement
 
