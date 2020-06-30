a7dbe8aa7ba16e598f472eb5190843350f6bf488
Ify <Zasterify@gmail.com>
2020-06-30 18:28:09 -0400

    remaining

--- Numbers Practice/cookie.py
+++ Numbers Practice/cookie.py
@@ -1,6 +1,6 @@
 num_cookie_jar = int(input("How many cookies do you have in a jar: ")) # To enter the numbers of cookies as you like 
 num_people_room = int(input("How many people are in the room?: ")) # To count the number of people in the room 
 division = (int(num_cookie_jar // num_people_room)) # To calculate the answer from the above
-
+remainder = (int(num_cookie_jar % num_people_room)) # To count how many cookies are left 
 
 
