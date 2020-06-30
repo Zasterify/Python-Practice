70b4b2c7bbc12307ac127f531aa2dd85dfa73088
Ify <Zasterify@gmail.com>
2020-06-30 13:44:08 -0400

    fishes_caught

--- Numbers Practice/fishing.py
+++ Numbers Practice/fishing.py
@@ -1,4 +1,4 @@
 number_fishes_in_pond = int(input("How many do you want to catch: "))  # To put the number of fishes in a pond as you like
-
+number_fishes_caught = int(input("Enter a number: "))  # To store the numbers of fishes you already caught
 
 
