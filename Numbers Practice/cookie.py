7f6d16842d3acb8ac6bc88a943911d6a9f190810
Ify <Zasterify@gmail.com>
2020-06-30 18:26:09 -0400

    num_people

--- Numbers Practice/cookie.py
+++ Numbers Practice/cookie.py
@@ -1,1 +1,6 @@
 num_cookie_jar = int(input("How many cookies do you have in a jar: ")) # To enter the numbers of cookies as you like 
+num_people_room = int(input("How many people are in the room?: ")) # To count the number of people in the room 
+
+
+
+
