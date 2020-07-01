c6bc5047cf55e25562f4acb96ce079134500cacd
Ify <Zasterify@gmail.com>
2020-07-01 17:11:10 -0400

    Length and Index

---
+++ Console Output Practice/stringy.py
@@ -0,0 +1,75 @@
+name = "Fatima"
+count = len(name)
+first = "Fatima"[0]
+second = "Fatima"[1]
+third = "Fatima"[2]
+fourth = "Fatima"[3]
+print("The word is",name,"! Fatima has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "and index 3 is",fourth)
+
+age = "19"
+count = len(age)
+first = "age"[0]
+second = "age"[1]
+print("The word is",age,"! 19 has",count, "letters. The letter at index 0 is",first,", index 1 is",second)
+
+complexion = "Hispanic"
+count = len(complexion)
+first = "Hispanic"[2]
+second = "Hispanic"[3]
+third = "Hispanic"[4]
+fourth = "Hispanic"[5]
+print("The word is",complexion,"! Hispanic has",count, "letters. The letter at index 2 is",first,", index 3 is",second,",index 4 is",third, "and index  is",fourth)
+
+country = "United Kingdom"
+count = len(country) 
+first = "United Kingdom"[6] 
+second = "United Kingdom"[7]
+third = "United Kingdom"[8]
+fourth = "United Kingdom"[9]
+print("The word is",country,"! United Kingdom has",count, "letters. The letter at index 6 is",first,", index 7 is",second,",index 8 is",third, "and index 9 is",fourth)
+
+major = "Teacher"
+count = len(major)
+first = "Teacher"[3]
+second = "Teacher"[4]
+third = "Teacher"[5]
+fourth = "Teacher"[6]
+print("The word is",major,"! Teacher has",count, "letters. The letter at index 3 is",first,", index 4 is",second,",index 5 is",third, "and index 6 is",fourth)
+
+course = "Mathematics"
+count = len(course)
+first = "Teacher"[4]
+second = "Teacher"[5]
+third = "Teacher"[6]
+print("The word is",course,"! Mathematics has",count, "letters. The letter at index 4 is",first,", index 5 is",second,",index 6 is",third)
+
+hobbies = "Singing and Dancing"
+count = len(hobbies)
+first = "Singing and Dancing"[6]
+second = "Singing and Dancing"[9]
+third = "Singing and Dancing"[15]
+print("The word is",hobbies,"! Singing and Dancing has",count, "letters. The letter at index 6 is",first,", index 9 is",second,",index 15 is",third)
+
+color = "Violet"
+count = len(color)
+first = "Violet"[1]
+second = "Violet"[2]
+third = "Violet"[3]
+print("The word is",color,"! Violet has",count, "letters. The letter at index 1 is",first,", index 2 is",second,",index 3 is",third)
+
+college = "University of Birmingham"
+count = len(college)
+first = "University of Birmingham"[7]
+second = "University of Birmingham"[14]
+third = "University of Birmingham"[17]
+print("The word is",college,"! University of Birmingham has",count, "letters. The letter at index 7 is",first,", index 14 is",second,",index 17 is",third)
+
+movies = "Extraction"
+count = len(movies)
+first = "Extraction"[3]
+second = "Extraction"[5]
+third = "Extraction"[9]
+print("The word is",movies,"! Extraction has",count, "letters. The letter at index 3 is",first,", index 5 is",second,",index 9 is",third)
+
+
+
