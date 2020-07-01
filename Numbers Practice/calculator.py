1f2cc5e89d3895849d98af597e169c196f8f64ef
Ify <Zasterify@gmail.com>
2020-06-30 20:12:50 -0400

    check - in

--- Numbers Practice/calculator.py
+++ Numbers Practice/calculator.py
@@ -1,5 +1,5 @@
 meal = float(input("How much was your meal?: "))  # To put the price of meal
 tip = int(input("What percentage tip would you like to give?: ")) # To put the percentage whatever you want
 final_tip = (float(tip / 100 * meal))  # To calculate the final tip
-final_price = (float( tip + final_tip + meal - tip))  # To calculate the final price
+final_price = (float(final_tip + meal))  # To calculate the final price
 print("You will be tipping $",final_tip, " Your final total will be $",final_price, "Have a nice day!")  # To print the whole statement as the above
