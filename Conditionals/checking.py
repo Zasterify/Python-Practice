f3511ec5f6632ea0d33c75cdfb4ee02f4a3cb5e6
Ify <Zasterify@gmail.com>
2020-07-02 17:41:00 -0400

    odd_number

--- Conditionals/checking.py
+++ Conditionals/checking.py
@@ -3,4 +3,6 @@ similar = user  # To hold the variable as the above
 if user % 2 != 1: #  To put odd number which is exactly correct
     print("The number is wrong!") # If it returns even number and get wrong answer
 else:
+    print("The number is right!")  # If it returns odd number and get right answer
+
 
