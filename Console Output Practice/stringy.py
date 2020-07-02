af2376075abda766c180e15361126aaab009d3d3
Ify <Zasterify@gmail.com>
2020-07-01 22:22:52 -0400

    Upper and Lower

--- Console Output Practice/stringy.py
+++ Console Output Practice/stringy.py
@@ -6,13 +6,17 @@ third = name[2]
 fourth = name[3]
 fifth = name[4]
 sixth = name[5]
-print("The word is",name,"! Fatima has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, ",index 3 is",fourth,", index 4 is",fifth, "and index 5 is",sixth)
+up = name.upper()
+down = name.lower()
+print("The word is",name,"! Fatima has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, ",index 3 is",fourth,", index 4 is",fifth, "and index 5 is",sixth, "The upper case of the word is",up," and the lower case of the word is",down)
 
 age = "19"
 count = len(age)
 first = age[0]
 second = age[1]
-print("The word is",age,"! 19 has",count, "letters. The letter at index 0 is",first,", and index 1 is",second)
+up = age.upper()
+down = age.lower()
+print("The word is",age,"! 19 has",count, "letters. The letter at index 0 is",first,", and index 1 is",second, "The upper case of the word is",up," and the lower case of the word is",down)
 
 complexion = "Hispanic"
 count = len(complexion)
@@ -24,7 +28,9 @@ fifth = complexion[4]
 sixth = complexion[5]
 seventh = complexion[6]
 eighth = "Hispanic"[7]
-print("The word is",complexion,"! Hispanic has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, ", index 6 is",seventh, "and index 7 is",eighth)
+up = complexion.upper()
+down = complexion.lower()
+print("The word is",complexion,"! Hispanic has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, ", index 6 is",seventh, "and index 7 is",eighth, "The upper case of the word is",up," and the lower case of the word is",down) 
 
 country = "England"
 count = len(country) 
@@ -35,7 +41,9 @@ fourth = country[3]
 fifth = country[4]
 sixth = country[5]
 seventh = country[6]
-print("The word is",country,"! England has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, "and index 6 is",seventh)
+up = country.upper()
+down = country.lower()
+print("The word is",country,"! England has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, "and index 6 is",seventh, "The upper case of the word is",up," and the lower case of the word is",down)
 
 major = "Teacher"
 count = len(major)
@@ -46,7 +54,9 @@ fourth = major[3]
 fifth = major[4]
 sixth = major[5]
 seventh = major[6]
-print("The word is",major,"! Teacher has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, "and index 6 is",seventh)
+up = major.upper()
+down = major.lower()
+print("The word is",major,"! Teacher has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, "and index 6 is",seventh, "The upper case of the word is",up," and the lower case of the word is",down) 
 
 course = "Maths"
 count = len(course)
@@ -55,7 +65,9 @@ second = course[1]
 third = course[2]
 fourth = course[3]
 fifth = course[4]
-print("The word is",course,"! Maths has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth, "and index 4 is",fifth)
+up = course.upper()
+down = course.lower()
+print("The word is",course,"! Maths has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth, "and index 4 is",fifth, "The upper case of the word is",up," and the lower case of the word is",down) 
 
 hobbies = "Singing"
 count = len(hobbies)
@@ -66,7 +78,9 @@ fourth = hobbies[3]
 fifth = hobbies[4]
 sixth = hobbies[5]
 seventh = hobbies[6]
-print("The word is",hobbies,"! Singing has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, ", and index 6 is",seventh)
+up = hobbies.upper()
+down = hobbies.lower()
+print("The word is",hobbies,"! Singing has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, ", and index 6 is",seventh, "The upper case of the word is",up," and the lower case of the word is",down) 
 
 color = "Violet"
 count = len(color)
@@ -76,7 +90,9 @@ third = color[2]
 fourth = color[3]
 fifth = color[4]
 sixth = color[5]
-print("The word is",color,"! Violet has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, "and index 5 is",sixth)
+up = color.upper()
+down = color.lower()
+print("The word is",color,"! Violet has",count, "letters. The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, "and index 5 is",sixth, "The upper case of the word is",up," and the lower case of the word is",down) 
 
 movies = "Extraction"
 count = len(movies)
@@ -89,8 +105,10 @@ sixth = movies[5]
 seventh = movies[6]
 eighth = movies[7]
 ninth = movies[8]
-tenth = movies[9] 
-print("The word is",movies,"! Extraction has",count, "letters. The letter at index 0 is",first,", index 1 is",second,", index 2 is",third, ", index 3 is",fourth,", index 4 is",fifth,", index 5 is",sixth,", index 6 is",seventh, ", index 7 is",eighth,", index 8 is",ninth," and index 9 is",tenth)
+tenth = movies[9]
+up = movies.upper()
+down = movies.lower()
+print("The word is",movies,"! Extraction has",count, "letters. The letter at index 0 is",first,", index 1 is",second,", index 2 is",third, ", index 3 is",fourth,", index 4 is",fifth,", index 5 is",sixth,", index 6 is",seventh, ", index 7 is",eighth,", index 8 is",ninth," and index 9 is",tenth, "The upper case of the word is",up," and the lower case of the word is",down) 
 
 marital_status = "Single"
 count = len(marital_status)
@@ -100,7 +118,12 @@ third = marital_status[2]
 fourth = marital_status[3]
 fifth = marital_status[4]
 sixth = marital_status[5]
-print("The word is",marital_status, "! Single has",count, " letters. The The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth)
+up = marital_status.upper()
+down = marital_status.lower()
+print("The word is",marital_status, "! Single has",count, " letters. The The letter at index 0 is",first,", index 1 is",second,",index 2 is",third, "index 3 is",fourth,", index 4 is",fifth, ", index 5 is",sixth, "The upper case of the word is",up," and the lower case of the word is",down) 
+
+
+
 
 
 
