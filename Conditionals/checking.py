ce535a402fdec5f80aa6b00f27069efb48132477
Ify <Zasterify@gmail.com>
2020-07-02 17:38:00 -0400

    Either odd or even

--- Conditionals/checking.py
+++ Conditionals/checking.py
@@ -1,4 +1,4 @@
 user = int(input("Guess a number: ")) # To assume any number
 similar = user  # To hold the variable as the above
-
+if user % 2 != 1: #  To put odd number which is exactly correct
 
