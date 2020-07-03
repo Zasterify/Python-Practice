8789a0dd5590bbd020034cc1017cff8b3ccf8cee
Ify <Zasterify@gmail.com>
2020-07-03 12:10:59 -0400

    correction from updated

--- Average/age.py
+++ Average/age.py
@@ -1,4 +1,4 @@
-# This is the answer of number 3
+# This is the answer of number 3 
 name1 = int(input("Enter a number: ")) # To assume any number
 name2 = int(input("Enter a number: ")) # To put any number
 name3 = int(input("Enter a number: ")) # To access any number
