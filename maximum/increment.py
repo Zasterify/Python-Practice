87a8cfe71be1fbfb0a33a38065cf61b10580b8c0
Ify <Zasterify@gmail.com>
2020-07-03 12:02:45 -0400

    check_in

--- maximum/increment.py
+++ maximum/increment.py
@@ -1,6 +1,7 @@
+# This is the answer of number 2
 random = int(input('Enter a number: ')) # To put any number 
 random2 = int(input('Enter a number: ')) # To access any number
 if random > random2: # To know whether it is huge or not
-    print('The number is vast') # To print when the number is huge
+    print('The vast number is',random) # To print when the number is huge
 elif random2 > random: # To print whether the number is bigger or not
-    print('The number is bigger') # To print when the number is bigger
+    print('The bigger number is',random2) # To print when the number is bigger
