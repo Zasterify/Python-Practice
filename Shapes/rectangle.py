16329cab1990e067574c762e544fee602fabda52
Ify <Zasterify@gmail.com>
2020-07-03 12:09:08 -0400

    updated 2

--- Shapes/rectangle.py
+++ Shapes/rectangle.py
@@ -1,3 +1,4 @@
+# This is the answer of number 1 
 user = int(input("Enter a length: "))  # To enter any size for the length 
 user2 = int(input("Enter a breadth: ")) # To access any size for the breadth
 if user == user2: # To observe whether it is equal or not.
