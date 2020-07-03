f17685aa92d7f05b1e1afafd144d6e6ed537fa3c
Ify <Zasterify@gmail.com>
2020-07-03 14:47:19 -0400

    changed

--- Average/age.py
+++ Average/age.py
@@ -2,7 +2,9 @@
 name1 = int(input("Enter a number: ")) # To assume any number
 name2 = int(input("Enter a number: ")) # To put any number
 name3 = int(input("Enter a number: ")) # To access any number
-if name1 < name2: # To check whether your age is older or not
-    print('Your age is youngest') # To print when your age is younger than other
-elif name3 > name2: # Same as line 4 
-    print('Your age is oldest') # To print when your age is older than others
+if name3 < name2 and name2 < name1:
+    print(name1,'is bigger')
+elif name3 < name1 and name1 < name2:
+    print(name2,'is bigger')
+elif name3 > name2 and name2 > name1:
+    print(name3, 'is bigger')
