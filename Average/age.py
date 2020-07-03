dc192da66b1483791de583671ff1f8d11981b3e6
Ify <Zasterify@gmail.com>
2020-07-03 10:57:01 -0400

    number_three

--- Average/age.py
+++ Average/age.py
@@ -1,2 +1,3 @@
 name1 = int(input("Enter a number: ")) # To assume any number
 name2 = int(input("Enter a number: ")) # To put any number
+name3 = int(input("Enter a number: ")) # To access any number
