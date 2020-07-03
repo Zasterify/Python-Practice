0bc1c615da0a1684cc7eb95c903018941efe0c26
Ify <Zasterify@gmail.com>
2020-07-03 15:46:42 -0400

    correction

--- Average/age.py
+++ Average/age.py
@@ -3,8 +3,8 @@ name1 = int(input("Enter a number: ")) # To assume any number
 name2 = int(input("Enter a number: ")) # To put any number
 name3 = int(input("Enter a number: ")) # To access any number
 if name3 < name2 and name2 < name1:
-    print(name1,'is bigger')
+    print(name1 + ' is bigger')
 elif name3 < name1 and name1 < name2:
     print(name2,'is bigger')
-elif name3 > name2 and name2 > name1:
-    print(name3, 'is bigger')
+elif name3 > name2 and name1 < name3:
+    print(name3,'is bigger')
