11e28b7ff32395d3748cd7166702ac359351e458
Ify <Zasterify@gmail.com>
2020-07-03 11:41:45 -0400

    either

--- maximum/increment.py
+++ maximum/increment.py
@@ -1,2 +1,3 @@
 random = int(input('Enter a number: ')) # To put any number 
 random2 = int(input('Enter a number: ')) # To access any number
+if random > random2: # To know whether it is huge or not
