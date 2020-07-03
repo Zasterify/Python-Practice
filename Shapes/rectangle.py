5892184e2a6bd028a1e8ea07fca6907802d89d35
Ify <Zasterify@gmail.com>
2020-07-03 10:08:47 -0400

    print_square

--- Shapes/rectangle.py
+++ Shapes/rectangle.py
@@ -1,3 +1,4 @@
 user = int(input("Enter a length: "))  # To enter any size for the length 
 user2 = int(input("Enter a breadth: ")) # To access any size for the breadth
 if user == user2: # To observe whether it is equal or not.
+    print("The rectangle is a square.") # To print if they are excatly the same
