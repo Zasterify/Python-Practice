8290638be8bfe402fa24c896ad17815f502e9834
Ify <Zasterify@gmail.com>
2020-07-03 11:42:53 -0400

    print_huge

--- maximum/increment.py
+++ maximum/increment.py
@@ -1,3 +1,4 @@
 random = int(input('Enter a number: ')) # To put any number 
 random2 = int(input('Enter a number: ')) # To access any number
 if random > random2: # To know whether it is huge or not
+    print('The number is vast') # To print when the number is huge
