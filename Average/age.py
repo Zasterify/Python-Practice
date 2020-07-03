175796e34e8930235c8e0c3d5fd0e7cfcefb6472
Ify <Zasterify@gmail.com>
2020-07-03 10:59:09 -0400

    youngest

--- Average/age.py
+++ Average/age.py
@@ -2,3 +2,4 @@ name1 = int(input("Enter a number: ")) # To assume any number
 name2 = int(input("Enter a number: ")) # To put any number
 name3 = int(input("Enter a number: ")) # To access any number
 if name1 < name2: # To check whether your age is older or not
+    print('Your age is youngest') # To print when your age is younger than other
