1aaf834ae40f0026d60c392f7619a471e67c7b42
Ify <Zasterify@gmail.com>
2020-07-03 21:54:13 -0400

    changed_number

--- Average/age.py
+++ Average/age.py
@@ -2,11 +2,21 @@
 name1 = int(input("Enter a number: ")) # To assume any number
 name2 = int(input("Enter a number: ")) # To put any number
 name3 = int(input("Enter a number: ")) # To access any number
-if name3 < name2 and name2 < name1:
-    print(name1,'is biggest and' ,name3, 'is smallest')
-elif name3 < name1 and name1 < name2:
-    print(name2,'is biggest and' ,name3, 'is smallest')
+if name1 > name2 and name2 > name3:
+    print(name1,' is greater and', name3, 'is smaller')
+elif name1 > name3 and name3 > name2:
+    print(name1, 'is greater and', name2,'is smaller')
+
+elif name2 > name3 and name3 > name1:
+     print(name2,' is greater and', name1, 'is smaller')
+elif name2 > name1 and name1 > name3:
+    print(name2,' is greater and', name3, 'is smaller')
+
 elif name3 > name2 and name2 > name1:
-    print(name3,'is biggest and' ,name1, 'is smallest')
+    print(name3,' is greater and', name1, 'is smaller')
+
+elif name3 > name1 and name1 > name2:
+     print(name3,' is greater and', name2, 'is smaller')
+
 else:
-    print(name3,'is biggest and' ,name2, 'is smallest')
+    print('all the number is the same.')
