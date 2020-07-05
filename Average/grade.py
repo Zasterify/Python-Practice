a5bf78224c923a22d08e5dc48af9584ca55405ed
Ify <Zasterify@gmail.com>
2020-07-05 18:18:12 -0400

    print

--- Average/grade.py
+++ Average/grade.py
@@ -2,3 +2,4 @@ grade = int(input('Enter your grade: ')) # To ask user for the grade
 if grade >= 80:
     print('Your grade is A')  # If it is greater than 80 and return A
 elif grade >= 60:
+    print('Your grade is B')  # If it is greater than 60 and return B
