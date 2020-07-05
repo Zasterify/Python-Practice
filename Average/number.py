12c2114250b4a50b61f6950a017c7f8d144cb9cb
Ify <Zasterify@gmail.com>
2020-07-05 19:55:36 -0400

    print

--- Average/number.py
+++ Average/number.py
@@ -2,3 +2,4 @@ num = int(input('Enter a number: ')) # To ask user for any number
 if num % 5 and 3 == 0:
     print('FizzBiss')  # If it is divided by both and return FizzBiss
 elif num % 5 == 0:
+    print('Fizz')  # If it is divided by 5 and return Fizz
