a916aaddfd7a73c384dde3782aeb7f3d13b0b34f
Ify <Zasterify@gmail.com>
2020-07-05 19:56:31 -0400

    biss

--- Average/number.py
+++ Average/number.py
@@ -3,3 +3,5 @@ if num % 5 and 3 == 0:
     print('FizzBiss')  # If it is divided by both and return FizzBiss
 elif num % 5 == 0:
     print('Fizz')  # If it is divided by 5 and return Fizz
+
+elif num % 3 == 0:
