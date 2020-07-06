0c87f1662d521002ac4df27c6a72341dbc9fecd0
Ify <Zasterify@gmail.com>
2020-07-06 17:19:58 -0400

    updated

--- Average/t-age.py
+++ Average/t-age.py
@@ -3,7 +3,7 @@ if age < 18:
     print('You are a minor')
     print('You cannot work legally!')  # If your age is younger than 18 and return like this
 else:
-    if age >= 18 and age <= 50: 
+    if age >= 18 and age <= 60: 
         print('You are an adult')
         print('You can work legally')   # If your age is older than 18 and return like this
     else:
