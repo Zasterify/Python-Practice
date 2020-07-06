a2bfdd5fcbe8482f4e51850afe62e30bf903dff4
Ify <Zasterify@gmail.com>
2020-07-06 16:04:28 -0400

    print_two

--- Average/t-age.py
+++ Average/t-age.py
@@ -1,3 +1,4 @@
 age = int(input('Enter your age: ')) # To put your age
 if age < 18:
     print('You are a minor')
+    print('You cannot work legally!')  # If your age is younger than 18 and return like this
