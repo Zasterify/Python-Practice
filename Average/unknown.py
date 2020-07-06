031bd694bfd6a9a75a850c69282e6929d6c2d868
Ify <Zasterify@gmail.com>
2020-07-06 00:53:13 -0400

    updated

--- Average/unknown.py
+++ Average/unknown.py
@@ -2,17 +2,18 @@ num = int(input('Enter a number: '))
 if num % 2 == 0:
     if num <= 50:
         print('Low Even')
-    if num >= 51:
+    if num >= 51 and num <= 99:
         print('High Even')
-else:
-    print('Unknown Even')
+    else:
+        print('Unknown Even')
 if num % 2 == 1:
     if num <= 50:
         print('Low Odd')
-    if num >= 51:
+    if num >= 51 and num <= 99:
         print('High Odd')
 else:
     print('Unknown Odd')
+
 
 
 
