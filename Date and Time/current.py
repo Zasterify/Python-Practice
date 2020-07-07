066b9112eb632abff009a7b611ebafa520ed4e65
Ify <Zasterify@gmail.com>
2020-07-07 15:59:41 -0400

    date-time

--- Date and Time/current.py
+++ Date and Time/current.py
@@ -1,24 +1,3 @@
 from datetime import datetime
 present = datetime.now()
 print (present)  # current date and time
-
-print (present.year)  # current year
-print (present.month)  # current month
-print (present.day)  # current day
-
-print (present.weekday())  # Weekday of the week
-
-present = datetime.now()
-print (present.strftime('%A'))  # Day of the week
-
-present = datetime.now()
-print (present.strftime('%B'))  # Month of the year
-
-present = datetime.now()
-print (present.strftime('%D'))  # Day of the month
-
-present = datetime.now()
-print (present.strftime('%-j'))  # Day of the year
-
-present = datetime.now()
-print (present.strftime('%w'))  # Week number of the year
