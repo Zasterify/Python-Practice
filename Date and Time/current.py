9885c7413ad57adfe86fd342bea59648f7f66dfb
Ify <Zasterify@gmail.com>
2020-07-07 16:03:36 -0400

    week-year

--- Date and Time/current.py
+++ Date and Time/current.py
@@ -6,3 +6,6 @@ print (present.year)  # current year
 
 present = datetime.now()
 print (present.strftime('%B'))  # Month of the year
+
+import datetime
+print(datetime.date(2020, 7, 7).isocalendar()[1])  # Week number of the year
