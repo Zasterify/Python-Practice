44a219348d941ea8521addf01f48d6d90551630b
Ify <Zasterify@gmail.com>
2020-07-07 16:06:43 -0400

    day-year

--- Date and Time/current.py
+++ Date and Time/current.py
@@ -11,3 +11,7 @@ import datetime
 print(datetime.date(2020, 7, 7).isocalendar()[1])  # Week number of the year
 
 print (present.weekday())  # Weekday of the week
+
+from datetime import datetime
+present = datetime.now()
+print (present.strftime('%-j'))  # Day of the year
