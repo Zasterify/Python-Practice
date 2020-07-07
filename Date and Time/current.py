938bc26999723094c78a5918ee4bb0b95b317bc7
Ify <Zasterify@gmail.com>
2020-07-07 16:01:25 -0400

    month-year

--- Date and Time/current.py
+++ Date and Time/current.py
@@ -3,3 +3,6 @@ present = datetime.now()
 print (present)  # current date and time
 
 print (present.year)  # current year
+
+present = datetime.now()
+print (present.strftime('%B'))  # Month of the year
