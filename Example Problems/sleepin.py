2e3e5864676893e98bdeb69634c3f756d472e052
Ify <Zasterify@gmail.com>
2020-07-08 11:53:16 -0400

    print sleep_in

--- Example Problems/sleepin.py
+++ Example Problems/sleepin.py
@@ -8,3 +8,7 @@ def sleep_in(weekday, vacation):  # To define the function 'sleep_in' consits wi
         return True  # You can sleep in
     else:            # if stuck
         return False # You can't sleep in
+
+print(sleep_in(False, True)) # You have to call your function at the end 
+# for it to do things. It is like making a calculator. You have made the calculator,
+# Now you have to give the calculator things to calculate
