65d15c11185694bc87438a18ba3857fc006ce174
Ify <Zasterify@gmail.com>
2020-07-08 17:57:16 -0400

    allowed

--- Example Problems/permissions.py
+++ Example Problems/permissions.py
@@ -1,2 +1,3 @@
 def permission(dad, mom):  # To define the function 'permission' with 2 arguments 'dad' and 'mom'
     if dad and mom:  # if mom and dad are permitted Deborah to play soccer 
+        return True   # if permission is true, then she is allowed to play it
