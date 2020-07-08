f17ad7cad81a3d11a39dda9283f4d3ac5e7932ac
Ify <Zasterify@gmail.com>
2020-07-08 11:46:43 -0400

    else

--- Example Problems/sleepin.py
+++ Example Problems/sleepin.py
@@ -1,3 +1,4 @@
 def sleep_in(weekday, vacation):  # To define the function 'sleep_in' consits with 'weekday' and 'vacation'
     if not weekday:  # If it is not weekday 
         return True  # You can only sleep in
+    else:            # if stuck
