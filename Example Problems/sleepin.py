776f302b3d5b82320b0d78aaf443c4a6bdb6479f
Ify <Zasterify@gmail.com>
2020-07-08 11:45:45 -0400

    true

--- Example Problems/sleepin.py
+++ Example Problems/sleepin.py
@@ -1,2 +1,3 @@
 def sleep_in(weekday, vacation):  # To define the function 'sleep_in' consits with 'weekday' and 'vacation'
     if not weekday:  # If it is not weekday 
+        return True  # You can only sleep in
