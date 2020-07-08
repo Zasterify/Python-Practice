2e8cb04d9e3995671e952717400299e3a3575afb
Ify <Zasterify@gmail.com>
2020-07-08 18:11:29 -0400

    added multi-string

--- Example Problems/permissions.py
+++ Example Problems/permissions.py
@@ -1,4 +1,7 @@
-def permission(dad, mom):  # To define the function 'permission' with 2 arguments 'dad' and 'mom'
+def permission(dad, mom):  
+    """
+    To define the function 'permission' with 2 arguments 'dad' and 'mom' 
+    """
     if dad and mom:  # if mom and dad are permitted Deborah to play soccer 
         return True   # if permission is true, then she is allowed to play it
     else:
