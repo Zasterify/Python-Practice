9fd6399c3bb1dfe1138b59d3a769eaadfec55fcf
Ify <Zasterify@gmail.com>
2020-07-08 22:14:00 -0400

    not_covid

--- Example Problems/school.py
+++ Example Problems/school.py
@@ -2,6 +2,6 @@ def open_school(covid, school, tuition):
     """
     To define the function 'open_school' with three 3 arguments 'covid' 'school' and 'tuition' 
     """
-    
+    if not covid and school and tuition:  # if covid is false, school is true and tuition is true
 
 
