3273e309aa3a2b007c60a5ce912ce4c93772b0bf
Ify <Zasterify@gmail.com>
2020-07-09 15:25:51 -0400

    if equal

--- Example Problems/sum double.py
+++ Example Problems/sum double.py
@@ -3,3 +3,4 @@ def sum_double(value1, value2):
     To define the function 'sum_double' with 2 given arguments
     '''
     sum = value1 + value2  # To calculate the sum of value1 and valiue2 
+    if value1 == value2:  # if the values are the same or not
