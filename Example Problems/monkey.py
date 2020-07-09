f80bc375778288f6183c4d026effa381fafe93b3
Ify <Zasterify@gmail.com>
2020-07-09 01:16:38 -0400

    if smiling

--- Example Problems/monkey.py
+++ Example Problems/monkey.py
@@ -2,4 +2,4 @@ def monkey_trouble(a_smile, b_smile):
     """
     To define the function 'monkey_trouble' with 2 agruments 'a_smile' and 'b_smile' 
     """
-    
+     if a_smile and b_smile or not a_smile and not b_smile:  # if both are smiling or not
