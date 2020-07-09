fcfa02c4ed183d4a7fd81568db9cdc99e2f12cd7
Ify <Zasterify@gmail.com>
2020-07-09 18:46:55 -0400

    updated comment

--- Example Problems/diff.py
+++ Example Problems/diff.py
@@ -2,11 +2,11 @@ def diff21(n):
     '''
     To define the function 'diff21' with one agrument 'n'
     '''
-    if n >= 21:  # if n is greater than 21 or less than 21
-        return (n - 21) * 2  # To return double if n is greater than 21
+    if n >= 21:  # To notice whether n is greater than 21 or less than 21
+        return (n - 21) * 2  # To get the answer which is double if greater than 21
     else:
-        return 21 - n  # To return if n is least than 21
+        return 21 - n  # To get the answer if least than 21
 
-print(diff21(10))  # To print this to caal the function 'diff21'
+print(diff21(10))  # To print this to call the function 'diff21'
 
 
