5e7e07773011115af6d71cc8a0fdb29eb39dc383
Ify <Zasterify@gmail.com>
2020-07-08 22:17:12 -0400

    school

--- Example Problems/school.py
+++ Example Problems/school.py
@@ -4,5 +4,7 @@ def open_school(covid, school, tuition):
     """
     if not covid and school and tuition:  # if covid is false, school is true and tuition is true
         return ('SCHOOL IS OPEN FOR IN PERSON INSTRUCTION')  # To print this statement
+    
+    if not covid and school and not tuition:  # if covid is false, school is true and tuition is false
 
 
