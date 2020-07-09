df76cc954911fe80ae60e09f887760c4ebd67070
Ify <Zasterify@gmail.com>
2020-07-08 22:15:38 -0400

    return_one

--- Example Problems/school.py
+++ Example Problems/school.py
@@ -3,5 +3,6 @@ def open_school(covid, school, tuition):
     To define the function 'open_school' with three 3 arguments 'covid' 'school' and 'tuition' 
     """
     if not covid and school and tuition:  # if covid is false, school is true and tuition is true
+        return ('SCHOOL IS OPEN FOR IN PERSON INSTRUCTION')  # To print this statement
 
 
