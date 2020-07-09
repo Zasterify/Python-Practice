8268a1eeff4ce5707d222f8d4ba48294e82072a2
Ify <Zasterify@gmail.com>
2020-07-09 16:28:59 -0400

    return this

--- Example Problems/diff.py
+++ Example Problems/diff.py
@@ -3,3 +3,4 @@ def diff21(n):
     To define the function 'diff21' with one agrument 'n'
     '''
     if n >= 21:  # if n is greater than 21 or not
+        return n - 21  # To return if n is greater than 21
