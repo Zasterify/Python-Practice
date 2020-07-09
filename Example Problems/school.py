3ae16fbab43d6002afb2f0cbf6e1d14f1f2e9a6c
Ify <Zasterify@gmail.com>
2020-07-08 22:21:01 -0400

    return_three

--- Example Problems/school.py
+++ Example Problems/school.py
@@ -9,5 +9,6 @@ def open_school(covid, school, tuition):
         return ('SCHOOL IS OPEN, BUT YOU HAVE NOT PAID')  # To print this statement
 
     if covid and not school and tuition:  # if covid is true, school is false and tuition is true
+        return ('SCHOOL IS CLOSED FOR IN PERSON INSTRUCTION. PLEASE STAY HOME FOR ONLINE LEARNING')  # To print this statement
 
 
