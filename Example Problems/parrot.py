22bb6c7a5f97da6f9675a4dda750e10525d82b79
Ify <Zasterify@gmail.com>
2020-07-09 19:41:27 -0400

    hour spent

--- Example Problems/parrot.py
+++ Example Problems/parrot.py
@@ -2,4 +2,4 @@ def parrot_trouble(parrot_talking, hour):
     '''
     To define the function which is called parrot_trouble with two agruments
     '''
-    
+    hour <= 23  # It only takes less than 24 hours in a day
