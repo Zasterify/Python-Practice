d8c48fb9c57789dcab9c4b746d22089c10f70167
Ify <Zasterify@gmail.com>
2020-07-08 23:45:32 -0400

    updated

--- Example Problems/school.py
+++ Example Problems/school.py
@@ -5,18 +5,17 @@ def open_school(covid, school, tuition):
     if not covid and school and tuition:  # if covid is false, school is true and tuition is true
         return ('SCHOOL IS OPEN FOR IN PERSON INSTRUCTION')  # To print this statement
     
-    if not covid and school and not tuition:  # if covid is false, school is true and tuition is false
+    elif not covid and school and not tuition:  # if covid is false, school is true and tuition is false
         return ('SCHOOL IS OPEN, BUT YOU HAVE NOT PAID')  # To print this statement
 
-    if covid and not school and tuition:  # if covid is true, school is false and tuition is true
+    elif covid or not school and tuition:  # if covid is true, school is false and tuition is true
         return ('SCHOOL IS CLOSED FOR IN PERSON INSTRUCTION. PLEASE STAY HOME FOR ONLINE LEARNING')  # To print this statement
 
-    if covid and school and tuition:  # if covid is true, school is true and tuition is false
+    elif covid or school or tuition:  # if covid is true, school is true and tuition is true
         return ('SCHOOL IS CLOSED FOR IN PERSON INSTRUCTION. PLEASE STAY HOME FOR ONLINE LEARNING')  # To print this statement
-
-    if not covid and not school and not tuition:  # if covid is false, school is false and tuition is false
+    else:  # if covid is false, school is false and tuition is false
         return ('SEEK FURTHER INSTRUCTION BY CONTACTING THE SCHOOL') # To print this statement
 
-print(open_school(False, False, False))  # To print this to call the function 'open_school'
+print(open_school(True, True, True))  # To print this to call the function 'open_school'
 
 
