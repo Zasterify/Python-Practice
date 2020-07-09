943e152682a88cdc1ab312fa251ae07f92e292b5
Ify <Zasterify@gmail.com>
2020-07-09 19:44:55 -0400

    return False

--- Example Problems/parrot.py
+++ Example Problems/parrot.py
@@ -6,3 +6,4 @@ def parrot_trouble(parrot_talking, hour):
     if parrot_talking and hour <= 7 or parrot_talking and hour >= 20: # Observing whether parrot talking spent either less 7 hours or more than 20 hours
         return True  # To prove how parrot talking spent is true
     else:
+        return False  # To get evidence how it is false
