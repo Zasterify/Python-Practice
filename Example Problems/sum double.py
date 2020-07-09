56af5bb033a053507b1e0c65d0c830bedf4bed22
Ify <Zasterify@gmail.com>
2020-07-09 15:25:12 -0400

    sum of values

--- Example Problems/sum double.py
+++ Example Problems/sum double.py
@@ -2,4 +2,4 @@ def sum_double(value1, value2):
     '''
     To define the function 'sum_double' with 2 given arguments
     '''
-    
+    sum = value1 + value2  # To calculate the sum of value1 and valiue2 
