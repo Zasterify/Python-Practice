d385ec856bd0fb7e83b2fb11edda1d7c6e636f0d
Ify <Zasterify@gmail.com>
2020-07-10 15:27:18 -0400

    both

--- Example Problems/posit and negat.py
+++ Example Problems/posit and negat.py
@@ -2,4 +2,4 @@ def pos_neg(value1, value2, both):
     '''
     To define the function 'pos_neg' with 2 arguments in one parameter
     ''' 
-    
+    both = value1 and value2  # if two values are the same 
