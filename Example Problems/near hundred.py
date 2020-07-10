c6214bc09744af4681c1524cf094844d89245d91
Ify <Zasterify@gmail.com>
2020-07-09 23:55:13 -0400

    if_absolute

--- Example Problems/near hundred.py
+++ Example Problems/near hundred.py
@@ -2,4 +2,4 @@ def near_hundred(n):
     '''
     To define the function 'near_hundred' with one argument 'n'
     '''
-    
+    if abs(n - 100) <= 10 or abs(n - 200) <= 10:  # to discover the answer whether n is less than 10 or more than 10
