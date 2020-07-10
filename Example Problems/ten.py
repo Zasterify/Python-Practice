089e8ff93c55e50bd4a75fb9d777feb5e73a8f74
Ify <Zasterify@gmail.com>
2020-07-09 20:51:22 -0400

    if a_b or sum

--- Example Problems/ten.py
+++ Example Problems/ten.py
@@ -3,6 +3,7 @@ def makes10(a, b):
     To define the function which is called 'makes10' with 2 arguments 'a' and 'b' 
     '''
     sum = a + b  # To make a total of a and b
+    if a == 10 or b == 10 or sum == 10:  # To notice which one of them that can give 10
 
 
 
