58487aa01918dbc4aded901da3e42295f4530db5
Ify <Zasterify@gmail.com>
2020-07-09 20:08:42 -0400

    updated

--- Example Problems/parrot.py
+++ Example Problems/parrot.py
@@ -2,7 +2,6 @@ def parrot_trouble(parrot_talking, hour):
     '''
     To define the function which is called parrot_trouble with two agruments
     '''
-    hour <= 23  # It only takes less than 24 hours in a day
     if parrot_talking and hour <= 7 or parrot_talking and hour >= 20: # Observing whether parrot talking spent either less 7 hours or more than 20 hours
         return True  # To prove how parrot talking spent is true
     else:
