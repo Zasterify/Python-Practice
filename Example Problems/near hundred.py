eff4430ac43443c066f91221983ff1f05748e343
Ify <Zasterify@gmail.com>
2020-07-09 23:56:55 -0400

    or

--- Example Problems/near hundred.py
+++ Example Problems/near hundred.py
@@ -4,3 +4,5 @@ def near_hundred(n):
     '''
     if abs(n - 100) <= 10 or abs(n - 200) <= 10:  # to discover the answer whether n is less than 10 or more than 10
          return True  # if the answer is almost 10
+    else:
+        
