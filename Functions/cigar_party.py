6118818c4d771a1380df9f8d5266eb2d0937d432
Ify <Zasterify@gmail.com>
2020-07-13 19:34:56 -0400

    if num_cigar

--- Functions/cigar_party.py
+++ Functions/cigar_party.py
@@ -1,3 +1,3 @@
 def cigar_party(num_cigar,weekend):  
     '''to define the function called cigar_party with 2 parameters'''
-    
+    if num_cigar >= 40 and num_cigar <=60 or not weekend == False:  # to notice whether num of cigars is between 40 and 60 or if not weekend
