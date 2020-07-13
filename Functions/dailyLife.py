cdc940d3ef253fa386673ed57c99ede485884fb9
Ify <Zasterify@gmail.com>
2020-07-12 21:50:15 -0400

    return price

--- Functions/dailyLife.py
+++ Functions/dailyLife.py
@@ -12,3 +12,4 @@ def uber(price):
 
 def total(shop_price, rent_price, uber_price):
     '''to define the function called 'total' with 3 parameters'''
+    return shopping(shop_price) + rent(rent_price) + uber(uber_price)  # to calculate the amount of prices 
