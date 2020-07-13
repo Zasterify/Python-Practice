963a7188ed5dbf5d11235de2579c270f7dddd744
Ify <Zasterify@gmail.com>
2020-07-13 12:23:46 -0400

    define word_two

--- Functions/funcHello.py
+++ Functions/funcHello.py
@@ -2,5 +2,6 @@ def word1():
     '''to define the function called 'word1' '''
     print ('Hello')  # to print inside of the function
 
-
+def word2(): 
+    '''to define the function called 'word2' '''
 
