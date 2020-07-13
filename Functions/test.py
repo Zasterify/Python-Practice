695d01c88f805073ecbaf560c7fecb9a32cc08bd
Ify <Zasterify@gmail.com>
2020-07-13 17:29:19 -0400

    define_func(y)

--- Functions/test.py
+++ Functions/test.py
@@ -2,4 +2,5 @@ def func1(x):
     '''to define the function called func1 with a parameter'''
     return x * 1/2  # to calculate the value of x
 
-
+def func2(y):  
+    '''to define the function called func2 with a parameter'''
