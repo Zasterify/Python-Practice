eaa2cb6145c6be51fd0540143af87288c34fb123
Ify <Zasterify@gmail.com>
2020-07-12 21:46:02 -0400

    define_rent

--- Functions/dailyLife.py
+++ Functions/dailyLife.py
@@ -2,3 +2,5 @@ def shopping(price):
     '''to define the function called 'shopping' with 1 parameter'''
     return price * 2
 
+def rent(price): 
+    '''to define the function called 'rent' with 1 parameter'''
