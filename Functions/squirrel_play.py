12aff1d3919aa4e646fa8591cd148a81b02cede6
Ify <Zasterify@gmail.com>
2020-07-14 02:07:31 -0400

    if summer

--- Functions/squirrel_play.py
+++ Functions/squirrel_play.py
@@ -2,4 +2,5 @@ def squirrel_play(temperature, summer):
     '''to define the function called squirrel_play with 2 agruments'''
     if temperature >= 60 and temperature <= 90 and not summer:  # to check whether temperature is greater than 60 and less than 90 and not summer
         return True
+    elif summer and temperature > 70:  # if summer and temperature is greater than 70
 
