36f39521b66147cf3d7adc329f19b67112003f27
Ify <Zasterify@gmail.com>
2020-07-14 00:46:09 -0400

    updated

--- Functions/date_fashion.py
+++ Functions/date_fashion.py
@@ -1,9 +1,9 @@
 def date_fashion(you, date):  
     '''to define the function called date_fashion with 2 parameter'''
-    if you >= 8 or date >= 8:  # if it is greater than 8
-        return 2  # to return yes
-    elif you <= 2 or date <= 2:  # if it is less than 2
+    if you <= 2 or date <= 2:  # if it is less than 2
         return 0  # to return no
+    elif you >= 8 or date >= 8:  # if it is greater than 8
+        return 2  # to return yes
     else:
         return 1  # to return maybe
 
