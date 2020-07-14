53921b683771536973a9576004c43135477e45ec
Ify <Zasterify@gmail.com>
2020-07-14 02:08:11 -0400

    return true

--- Functions/squirrel_play.py
+++ Functions/squirrel_play.py
@@ -3,4 +3,5 @@ def squirrel_play(temperature, summer):
     if temperature >= 60 and temperature <= 90 and not summer:  # to check whether temperature is greater than 60 and less than 90 and not summer
         return True
     elif summer and temperature > 70:  # if summer and temperature is greater than 70
+        return True
 
