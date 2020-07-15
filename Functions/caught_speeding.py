06ef5d7e93c6531c8af958d0b528fd4aed6525b6
Ify <Zasterify@gmail.com>
2020-07-15 12:14:48 -0400

    if speed

--- Functions/caught_speeding.py
+++ Functions/caught_speeding.py
@@ -1,3 +1,4 @@
 def caught_speeding(speed, birthday):  
     '''to define the function called caught_speeding with 2 arguements'''
+    if speed >= 81 and not birthday:  # if it is not birthday and speed is greater than 81
    
