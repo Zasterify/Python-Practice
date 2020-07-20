e637181ce3c48c19d560f65901b3fe5d3961b4a4
Ify <Zasterify@gmail.com>
2020-07-19 21:36:59 -0400

    increment

--- loop/while loop.py
+++ loop/while loop.py
@@ -2,3 +2,4 @@ fruits = ['Watermelon', 'Pawpaw', 'Pineapple', 'Pear', 'Grape']  # to mention 5 
 x = 0  # x is equal to 0 
 while x < len(fruits):  # while x is less than length of fruits
     print ('A new fruit is:',fruits[x])  # to print all the fruits from the above
+     x += 1  # x is to be increment
