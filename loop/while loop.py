c8a5eb5e6771fcd59599e1feb159d76cc616bd40
Ify <Zasterify@gmail.com>
2020-07-19 21:34:32 -0400

    while each in fruits

--- loop/while loop.py
+++ loop/while loop.py
@@ -1,2 +1,3 @@
 fruits = ['Watermelon', 'Pawpaw', 'Pineapple', 'Pear', 'Grape']  # to mention 5 random fruits
 x = 0  # x is equal to 0 
+while x < len(fruits):  # while x is less than length of fruits
