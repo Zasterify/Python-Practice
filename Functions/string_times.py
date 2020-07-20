91444cb2151de9725bd052445af25c2a208ae761
Ify <Zasterify@gmail.com>
2020-07-20 00:53:02 -0400

    return string

--- Functions/string_times.py
+++ Functions/string_times.py
@@ -2,5 +2,6 @@ def string_times(string, n):
     '''to define the function called string_times''' 
     if string and n == 2:
         return string * n  # if string is multiplied by 2
-    elif string and n == 3: 
+    elif string and n == 3:
+        return string * n  # if string is multiplied by 3 
     
