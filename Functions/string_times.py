66a5783f8feb82f9ea5d687fd692dc3c87e2789c
Ify <Zasterify@gmail.com>
2020-07-20 00:52:16 -0400

    if times is three

--- Functions/string_times.py
+++ Functions/string_times.py
@@ -1,5 +1,6 @@
 def string_times(string, n):  
     '''to define the function called string_times''' 
     if string and n == 2:
-        return string * n  # if string is multiplied by 2 
+        return string * n  # if string is multiplied by 2
+    elif string and n == 3: 
     
