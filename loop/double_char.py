385eb02ff15033529955bb4e10bf3fa338981db9
Ify <Zasterify@gmail.com>
2020-07-27 13:56:23 -0400

    result

--- loop/double_char.py
+++ loop/double_char.py
@@ -2,3 +2,4 @@ def double_char(word):
     '''to define the function called double_char with a given string'''
     result = ''
     for char in word:  # for each in word
+        result += char * 2  # to calculate for the result
