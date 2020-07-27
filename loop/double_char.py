2e8f002a868cf6b72b0ab96a976abe317114f08a
Ify <Zasterify@gmail.com>
2020-07-27 13:58:36 -0400

    print_result

--- loop/double_char.py
+++ loop/double_char.py
@@ -4,3 +4,5 @@ def double_char(word):
     for char in word:  # for each in word
         result += char * 2  # to calculate for the result
     return result  # to return doubled for each of them
+
+print(double_char('The'))  # to print for calling the function
