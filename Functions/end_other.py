3118d23359ce88263b882ef1a5ad90bcfe2a7a12
Ify <Zasterify@gmail.com>
2020-07-28 23:32:45 -0400

    return true

--- Functions/end_other.py
+++ Functions/end_other.py
@@ -3,3 +3,4 @@ def end_other(str1, str2):
     if str1[2:] == str2:  # if str1 at index 2 is equal to str2
         return True
     elif str2[2:] == str1: # if str2 at index 2 is equal to str2
+        return True
