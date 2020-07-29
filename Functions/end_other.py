95651f6d1b71a462c6d8351c2434efd27425bd70
Ify <Zasterify@gmail.com>
2020-07-28 23:32:02 -0400

    if str2 is equal to str1

--- Functions/end_other.py
+++ Functions/end_other.py
@@ -2,3 +2,4 @@ def end_other(str1, str2):
     ''' To define the function 'end_other' with only 2 arguments'''
     if str1[2:] == str2:  # if str1 at index 2 is equal to str2
         return True
+    elif str2[2:] == str1: # if str2 at index 2 is equal to str2
